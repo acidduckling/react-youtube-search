dd8caf6a7d42cf7e6da8ff793061aa3a1abdb525
Clayton Wakefield <acidduckling@gmail.com>
2018-07-28 21:48:37 +1000

    Retrieve videos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
 
 const API_KEY = 'AIzaSyBkbhJWwldKXi8utH2rYp62xGnOFabR6q8';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => this.setState({ videos }));
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
